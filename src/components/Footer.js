8bb8b997235db9cf8bf5f5ce7d2f094567ba5177
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-08-09 17:31:48 -0300

    create the component footer and styling it

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './footer.module.css'
+
+const Footer = ()=> {
+  return(
+    <div className={styles.divFooter}>
+      <footer>
+        <h4 class="copyright">Criado por: Carlos Eduardo</h4>
+        <Link to="https://facebook.com/cadunokia" target="_blank">
+          <img src="https://cdn-icons-png.flaticon.com/512/5968/5968764.png" alt="facebook icon"/></Link>
+        <Link to="https://linkedin.com/in/carlos-eduardo-704" target="_blank">
+          <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="linkedin icon"/></Link>
+        <Link to="https://github.com/carlosEduardo704" target="_blank">
+          <img src="https://cdn-icons-png.flaticon.com/512/733/733609.png" alt="github icon"/></Link>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
