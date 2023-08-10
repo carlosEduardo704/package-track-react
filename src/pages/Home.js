3373d51ce5e5959d1488c8a6398ae2a5c70ca758
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-08-10 00:30:52 -0300

    adding imports on Home.js and setting the global style on HomeStyle.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import './HomeStyle.css'
 
 // Components
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import CentralElements from '../components/CentralElements'
+import Section from '../components/Section'
 
 const Home = () => {
   return(
     <>
     <NavBar/>
+    <CentralElements/>
+    <Section/>
     <Footer/>
     </>
   )
