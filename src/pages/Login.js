cfd5863c926153a5baa0eae5e5062a0979525b1b
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-08-10 16:14:03 -0300

    building the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './LoginStyle.css'
 import Footer from '../components/Footer'
+import LoginPage from '../components/LoginRegisterPage/LoginPage'
+import NavBar from '../components/NavBar'
 
 const Login = () => {
   return(
     <>
+    <NavBar/>
+    <LoginPage/>
     <Footer/>
     </>
   )
