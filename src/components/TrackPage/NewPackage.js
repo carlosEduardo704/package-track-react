74e489de0b24a9071b4217a23b72b97878a66886
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-08-11 14:59:40 -0300

    create component NewPackage

---
+++ src/components/TrackPage/NewPackage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NewPackage = ()=> {
+  return (
+    <div>
+      <input type="text" placeholder="Novo Pacote"/>
+      <button>Pesquisar</button>
+    </div>
+    
+  )
+}
+
+export default NewPackage
