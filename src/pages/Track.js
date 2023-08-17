4ba4e6ed2687a17a36045428d5af9603a315cdff
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-08-17 19:07:49 -0300

    fixing page Track.js

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -3,13 +3,21 @@ import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import './TrackStyle.css'
 import NewPackage from '../components/TrackPage/NewPackage'
+import PackageCode from '../components/TrackPage/PackageCode'
+import HistoryPackage from '../components/TrackPage/HistoryPackage'
 
 const Track = () => {
   return(
-    <div className="trackExternalDiv">
-      <div className="trackInternalDiv">
+    <div >
+      <div className="trackExternalDivNavBar">
         <NavBar/>
+      </div>
+      <div className="trackInternalDiv">
         <NewPackage/>
+        <PackageCode/>
+        <HistoryPackage/>
+      </div>
+      <div className="trackExternalDivFooter">
         <Footer/>
       </div>
     </div>
