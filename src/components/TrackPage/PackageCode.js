d1be4eda8f566889dd867d8c047b2cbc85d50e1d
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-08-27 13:05:57 -0300

    creating component PackageCode and packageCode.module.css

---
+++ src/components/TrackPage/PackageCode.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './packageCode.module.css'
+
+const PackageCode = ()=> {
+  return (
+    <div className={styles.divPackageCode}>
+      <h2>AB123456789BR</h2>
+    </div>
+  )
+}
+
+export default PackageCode
