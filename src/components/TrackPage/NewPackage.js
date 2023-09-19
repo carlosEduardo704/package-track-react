3aaee218d81d458759b74f0b8d1d155b40d4cf18
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-09-19 07:31:13 -0300

    fixing element name

--- src/components/TrackPage/NewPackage.js
+++ src/components/TrackPage/NewPackage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from './newPackage.module.css'
-const NewPackage = ()=> {
+
+const NewPackage = () => {
   
   const [canIFetch, setCanIFetch] = useState(false);
   const [trackingCode, setTrackingCode] = useState('');
