5845758d27a87eb71104c46f1813e327a0c09c44
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-11-01 07:41:24 -0300

    fixing word 'Pagina'

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -11,7 +11,7 @@ const ErrorPage = () => {
     <NavBar/>
     <div className="divError">
       <h1 className="h1erro">Erro 404!</h1>
-      <h2 className="h2erro">Pagina não Encontrada!</h2>
+      <h2 className="h2erro">Página não Encontrada!</h2>
     </div>
     <div className="divFooter">
 
