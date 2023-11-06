66ea030a79a4d9047f8c509e522e9cd111c5a541
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-11-06 09:18:13 -0300

    adding line breaks in li elements

--- src/components/SearchCepPage/sectionElements.js
+++ src/components/SearchCepPage/sectionElements.js
@@ -13,10 +13,10 @@ const SectionElements = () =>{
         <ul className={styles.ulSectionElements}>
 
           <li className={styles.liSectionElements}>
-            Alameda: alameda pode ser considerada como uma rua arborizada. Devido à essa arborização, há passeios com sombras e praças com lugares fresco para a população.
+            Alameda: alameda pode ser considerada como uma rua arborizada. Devido à essa arborização, há passeios com sombras e <br/>praças com lugares fresco para a população.
           </li>
           <li className={styles.liSectionElements}>
-            Avenida: avenida pode ser interpretada como uma rua importante da cidade em questão. Na maioria das grandes cidades, avenidas possuem bastante movimento de pessoas e automóveis. Há também concentração de endereços comerciais. E por último, muitas avenidas fazem ligações importantes entre bairros e zonas das cidades.
+            Avenida: avenida pode ser interpretada como uma rua importante da cidade em questão. Na maioria das grandes cidades, <br/>avenidas possuem bastante movimento de pessoas e automóveis. Há também concentração de endereços comerciais. E por <br/>último, muitas avenidas fazem ligações importantes entre bairros e zonas das cidades.
           </li>
           <li className={styles.liSectionElements}>
             Beco: beco é uma pequena rua, com largura estreita. Geralmente o beco é uma rua sem saída e com iluminação bem fraca.
@@ -25,16 +25,16 @@ const SectionElements = () =>{
             Estrada: estradas são vias que geralmente ligam os municípios dentro do mesmo estado ou entre estados.
           </li>
           <li className={styles.liSectionElements}>
-            Praça: praça pode ser considerada como um espaço térreo cercada por 3 ou mais ruas. Geralmente há igrejas, comércios, sorveterias, pista de skate, jardins, árvores, bancos para descanso, quadras de esporte, ciclovia, aparelhos de exercícios e vários outras instalações utilizadas pela população.
+            Praça: praça pode ser considerada como um espaço térreo cercada por 3 ou mais ruas. Geralmente há igrejas, comércios, <br/>sorveterias, pista de skate, jardins, árvores, bancos para descanso, quadras de esporte, ciclovia, aparelhos de <br/>exercícios e vários outras instalações utilizadas pela população.
           </li>
           <li className={styles.liSectionElements}>
-            Quadra: o termo quadra ou a abreviação QD. Uma quadra é geralmente uma área delimitada por 4 ruas, avenidas ou praças. Muitas vezes utilizadas também para informar a distânciadistancia de uma rua a outra ex: em duas quadras há um hospital.
+            Quadra: o termo quadra ou a abreviação QD. Uma quadra é geralmente uma área delimitada por 4 ruas, avenidas ou praças. <br/>Muitas vezes utilizadas também para informar a distânciadistancia de uma rua a outra ex: em duas quadras há um <br/>hospital.
           </li>
           <li className={styles.liSectionElements}>
-            Rodovia: rodovia é considerada uma estrada maior e necessariamente é pavimentada para facilitar o tráfego de automóveis, motocicletas e caminhões a alta velocidade. No Brasil, a maioria dos produtos são transportados por caminhões que trafegam nas rodovias.
+            Rodovia: rodovia é considerada uma estrada maior e necessariamente é pavimentada para facilitar o tráfego de <br/>automóveis, motocicletas e caminhões a alta velocidade. No Brasil, a maioria dos produtos são transportados por <br/>caminhões que trafegam nas rodovias.
           </li>
           <li className={styles.liSectionElements}>
-            Travessa: travessa geralmente é a ligação de duas ruas maiores. A travessa na maioria dos casos possui apenas algumas dezenas de metros.
+            Travessa: travessa geralmente é a ligação de duas ruas maiores. A travessa na maioria dos casos possui apenas algumas <br/>dezenas de metros.
           </li>
           <li className={styles.liSectionElements}>
             Vila: vila pode ser considerada como um vilarejo mais afastado que ainda não foi considerado um município.
@@ -59,13 +59,13 @@ const SectionElements = () =>{
           Quarta zona postal é o estado de Minas Gerais, 30000-xxx a 39990-xxx.
           </li>
           <li className={styles.liSectionElements}>
-          Quinta zona postal: região dos estados da Bahia e Sergipe. A Bahia tem o CEP de 40000-xxx até 48999-xxx e o Sergipe de 49000-xxx a 4999-xxx.
+          <br/>Quinta zona postal: região dos estados da Bahia e Sergipe. A Bahia tem o CEP de 40000-xxx até 48999-xxx e o Sergipe de <br/>49000-xxx a 4999-xxx.
           </li>
           <li className={styles.liSectionElements}>
-          Sexta Zona Postal: composta por 4 estados que são; Pernambuco, Alagoas, Paraíba e Rio Grande Norte. Por Pernambuco, que tem o CEP começado por 50000-xxx a 56999-xxx, por Alagoas, 57000-xxx até 57999-xxx, por Paraíba, 58000-xxx até 58999-xxx e por Rio Grande do Norte, 59000-xxx até 59999-xxx.
+          Sexta Zona Postal: composta por 4 estados que são; Pernambuco, Alagoas, Paraíba e Rio Grande Norte. Por Pernambuco, que <br/>tem o CEP começado por 50000-xxx a 56999-xxx, por Alagoas, 57000-xxx até 57999-xxx, por Paraíba, 58000-xxx até 58999-xxx <br/>e por Rio Grande do Norte, 59000-xxx até 59999-xxx.
           </li>
           <li className={styles.liSectionElements}>
-          Sétima Zona Postal: composta por 8 estados que são; Ceará, Piauí, Maranhão, Pará, Amapá, Amazonas, Acre e Roraima. O Ceará, com CEP de 60000-xxx até 63990-xxx, o Piauí, 64000-xxx até 64990-xxx, o Maranhão, 65000-xxx até 65990-xxx, o Pará, 66000-xxx até 68890-xxx, o Amapá, 689000-xxx a 68999-xxx, o Amazonas, com CEP de 69000-xxx a 69299-xxx, pelo Acre, 69400-xxx a 69899-xxx e Roraima, 69300-xxx a 69399-xxx.
+          Sétima Zona Postal: composta por 8 estados que são; Ceará, Piauí, Maranhão, Pará, Amapá, Amazonas, Acre e Roraima. O <br/>Ceará, com CEP de 60000-xxx até 63990-xxx, o Piauí, 64000-xxx até 64990-xxx, o Maranhão, 65000-xxx até 65990-xxx, o <br/>Pará, 66000-xxx até 68890-xxx, o Amapá, 689000-xxx a 68999-xxx, o Amazonas, com CEP de 69000-xxx a 69299-xxx, pelo Acre, <br/>69400-xxx a 69899-xxx e Roraima, 69300-xxx a 69399-xxx.
           </li>
 
         </ul>
