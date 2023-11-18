dba0de020809c761510b9517fd122967b7345837
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-11-18 00:02:35 -0300

    creating the logic and adding the  api viaCep

--- src/components/SearchCepPage/centralElements.js
+++ src/components/SearchCepPage/centralElements.js
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from './centralElements.module.css'
-
+import BuscaCepDataContext from "./buscaCepContext";
 
 const CentralElementsSearchCep = () => {
 
   const [buscaCepCode, setBuscaCepCode] = useState('')
+
+  const [canIFetch, setCanIFetch] = useState(false);
+
+  const {data, updateData} = useContext(BuscaCepDataContext);
+
+  const buttonClick = () => {
+    setCanIFetch(true)
+  }
+
+  useEffect(()=>{
+    if(canIFetch){
+      async function fetchData(){
+        const apiLink = `https://viacep.com.br/ws/${buscaCepCode}/json/`
+        try{
+          const response = await fetch(apiLink);
+          const dataRequest = await response.json()
+          updateData(dataRequest)
+          console.log(dataRequest)
+          setCanIFetch(false)
+        }
+        catch(error){
+          alert('Cep inválido!')
+          setCanIFetch(false)
+        }
+      }
+      fetchData();
+    };
+  })
 
   return (
     <>
@@ -18,7 +46,7 @@ const CentralElementsSearchCep = () => {
             name="BuscaCepInput"
             placeholder="Digite seu CEP"
             onChange={(e) => setBuscaCepCode(e.target.value)}/>
-            <button className={styles.searchButton}>Pesquisar</button>
+            <button className={styles.searchButton} onClick={buttonClick}>Pesquisar</button>
         </div>
 
       </div>
