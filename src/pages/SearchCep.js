8e0470a5b24b1f6b975993f42cbd05460c36af0b
carlosEduardo704 <carlos704eduardo@gmail.com>
2023-11-17 23:59:58 -0300

    adding DataSearchCep element

--- src/pages/SearchCep.js
+++ src/pages/SearchCep.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavBar from '../components/NavBar';
 import CentralElementsSearchCep from '../components/SearchCepPage/centralElements';
 import Footer from '../components/Footer';
 import SectionElements from '../components/SearchCepPage/sectionElements';
+import BuscaCepDataContext from '../components/SearchCepPage/buscaCepContext';
+import DataSearchCep from '../components/SearchCepPage/DataSearchCep'
 
 const SearchCep = () => {
+
+  const {data, updateData} = useContext(BuscaCepDataContext);
   return(
     <div>
       <NavBar/>
       <CentralElementsSearchCep/>
+      {data && <DataSearchCep/>}
       <SectionElements/>
       <Footer/>
     </div>
   )
 }
 
-export default SearchCep
+export default SearchCep;
